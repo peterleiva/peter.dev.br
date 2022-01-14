63c97a5fe2187a7347cfc9b74ddf70c47ca61d81
Peter <github@peter.dev.br>
2022-01-14 03:50:05 -0300

    feat(resume): layout sections

--- packages/resume/src/components/Section.tsx
+++ packages/resume/src/components/Section.tsx
@@ -3,19 +3,54 @@ import type { ReactNode } from 'react';
 export type SectionProps = {
   title: string;
   children: ReactNode;
+  fill?: boolean;
 };
 
-export default function Section({ title, children }: SectionProps) {
-  return (
-    <section>
-      <h2>{title}</h2>
-      <div>{children}</div>
+const layoutClass = (fill: boolean) => (fill ? 'fill' : 'columns');
 
-      <style jsx>{`
-        section {
-          margin-block: var(--space-xl);
-        }
-      `}</style>
+export default function Section({
+  title,
+  children,
+  fill = false,
+}: SectionProps) {
+  return (
+    <section className={layoutClass(fill)}>
+      <h2>{title}</h2>
+      <div className="body">{children}</div>
+      <style jsx>
+        {`
+          h2 {
+            text-align: center;
+          }
+
+          .fill > .body {
+            text-align: justify;
+          }
+
+          section {
+            display: flex;
+            flex-flow: column;
+            gap: var(--space);
+          }
+
+          @media screen and (min-width: 560px) {
+            .columns {
+              display: grid;
+              grid-template-columns: repeat(4, 1fr);
+              gap: var(--gap);
+            }
+
+            .columns h2 {
+              text-align: left;
+              grid-column: 1 / 2;
+            }
+
+            .columns div {
+              grid-column: 2 / 5;
+            }
+          }
+        `}
+      </style>
     </section>
   );
 }
