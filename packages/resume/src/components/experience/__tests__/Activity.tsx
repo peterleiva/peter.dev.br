9d0ba217ca0ba0a31c8c5691205db907e7a0ab30
Peter <github@peter.dev.br>
2022-01-14 15:34:55 -0300

    test(resume): unit test experience's Activity component

---
+++ packages/resume/src/components/experience/__tests__/Activity.tsx
@@ -0,0 +1,30 @@
+import { render } from '@testing-library/react';
+import Activity from '../Activity';
+import { DateTime } from 'luxon';
+
+describe('Activity', () => {
+  test("renders 'present' when no activity", () => {
+    const { container } = render(<Activity />);
+
+    expect(container).toHaveTextContent(/present/i);
+  });
+
+  test('render Month and year of activity', () => {
+    const date = {
+      year: 1829,
+      month: 4,
+    };
+    const activity = DateTime.fromObject(date);
+
+    const { container } = render(<Activity time={activity} />);
+
+    expect(container).toHaveTextContent(activity.toFormat('LLL yyyy'));
+  });
+
+  test('render custom Present label', () => {
+    const label = '-';
+    const { container } = render(<Activity presentLabel={label} />);
+
+    expect(container).toHaveTextContent(label);
+  });
+});
