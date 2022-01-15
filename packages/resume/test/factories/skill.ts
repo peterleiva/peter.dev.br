ce42e18559b991d715b641248455399e6b3bbc2f
Peter <github@peter.dev.br>
2022-01-15 20:07:21 -0300

    test(chore): add skill factory

---
+++ packages/resume/test/factories/skill.ts
@@ -0,0 +1,22 @@
+import { Factory } from 'fishery';
+import casual from 'casual';
+import SkillModel, { Skill } from 'models/skill';
+import { times } from 'ramda';
+
+interface TransientParams {
+  tags: number;
+}
+const tagFactory = times(() => ({ name: casual.word }));
+
+export default Factory.define<Skill, TransientParams>(
+  ({ onCreate, transientParams: { tags: tagsQuantity = 1 } }) => {
+    onCreate(async skill => {
+      return await SkillModel.create(skill);
+    });
+
+    return {
+      name: casual.title,
+      tags: tagFactory(tagsQuantity),
+    };
+  }
+);
