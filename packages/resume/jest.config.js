a9e91c651d0702adb5b30afa0c5cb9332fb56703
Peter <github@peter.dev.br>
2022-01-15 20:03:56 -0300

    chore: fix jest.config moduleDirectories

--- packages/resume/jest.config.js
+++ packages/resume/jest.config.js
@@ -7,6 +7,7 @@ const factory = nextJest({
 const custom = {
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: ['node_modules', './', './src'],
 };
 
 module.exports = factory(custom);
