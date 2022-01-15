aab3808a95e539d612d64fde1ab9bad80e0fe133
Peter <github@peter.dev.br>
2022-01-15 20:08:48 -0300

    test(chore): make factory exportable

---
+++ packages/resume/test/factories/index.ts
@@ -0,0 +1,4 @@
+export { default as Skill } from './skill';
+export { default as Education } from './education';
+export { default as Job } from './job';
+export { default as Resume } from './resume';
