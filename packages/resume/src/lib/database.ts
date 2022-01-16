1c6b67b0be03bc0f90e3cfb35ee7a729daa7795c
Peter <github@peter.dev.br>
2022-01-16 13:09:10 -0300

    feat(resume): add database connect and disconnect

---
+++ packages/resume/src/lib/database.ts
@@ -0,0 +1,73 @@
+import mongoose, { Connection } from 'mongoose';
+
+export type DatabaseOptions = Partial<{
+  url: string;
+}>;
+
+const DATABASE_URL = process.env.DATABASE_URL;
+const isProd = process.env.NODE_ENV === 'production';
+const DEV_URI_FALLBACK = 'mongodb://localhost:27017/resume';
+
+export async function connect({
+  url = DATABASE_URL,
+}: DatabaseOptions = {}): Promise<Connection> {
+  if (!url && isProd) {
+    throw 'Database not set. Please, set DATABASE_URL environment variable';
+  }
+
+  url ??= DEV_URI_FALLBACK;
+
+  const { connection } = await mongoose.connect(url, {
+    appName: 'Resume',
+    wtimeoutMS: isProd ? 25_000 : 0,
+    socketTimeoutMS: 30_000 * 3,
+    maxPoolSize: 200,
+    keepAlive: true,
+    keepAliveInitialDelay: 300_000,
+  });
+
+  return connection;
+}
+
+export async function disconnect(connection: Connection): Promise<Connection> {
+  await connection.close();
+
+  return connection;
+}
+
+export async function cleanUp(connection: Connection): Promise<Connection> {
+  try {
+    await connection.dropDatabase();
+  } catch (err) {
+    console.error('Cannot clean the database');
+    console.error('%o', err);
+  }
+
+  return connection;
+}
+
+export async function log(connection: Connection): Promise<Connection> {
+  mongoose.connection.on('connected', function () {
+    const { host, port, name: db } = connection;
+    const connectionUri = `${host}:${port}/${db}`;
+    console.info(`Database connected at ${connectionUri}`);
+  });
+
+  mongoose.connection.on('disconnected', () => {
+    console.info(`🆘 Database lost connection`);
+  });
+
+  mongoose.connection.on('error', error => {
+    console.error(error);
+  });
+
+  return connection;
+}
+
+export async function effect<T>(fun: () => Promise<T>): Promise<T> {
+  const connection = await connect();
+  const result = await fun();
+  await disconnect(connection);
+
+  return result;
+}
