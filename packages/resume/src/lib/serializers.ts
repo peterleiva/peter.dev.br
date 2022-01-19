0d55fad96e5451b23523cde66f27d13d5bec71ed
Peter <github@peter.dev.br>
2022-01-19 10:26:50 -0300

    refactor(resume): add resume serializer and fix typing

--- packages/resume/src/lib/serializers.ts
+++ packages/resume/src/lib/serializers.ts
@@ -1,8 +1,14 @@
 import { optional, toISO } from './serialization-utils';
 import * as R from 'ramda';
 import { DateTime } from 'luxon';
+import { Contact } from 'models/resume';
+import { Resume, Job, Education, Courses } from 'types';
 
-export const jobSerializer = R.map(
+type SerializedJob = Omit<Job, 'activity'> & {
+  activity: { start: string; end?: string };
+};
+
+export const jobSerializer = R.map<Job, SerializedJob>(
   R.evolve({
     activity: {
       start: toISO,
@@ -11,7 +17,7 @@ export const jobSerializer = R.map(
   })
 );
 
-export const jobDeserializer = R.map(
+export const jobDeserializer = R.map<SerializedJob, Job>(
   R.evolve({
     activity: {
       start: DateTime.fromISO,
@@ -20,22 +26,61 @@ export const jobDeserializer = R.map(
   })
 );
 
-export const educationSerializer = R.map(
-  R.evolve({
-    started: toISO,
-    ended: optional(toISO),
-  })
+type SerializedEducation = Omit<Education, 'started' | 'ended'> & {
+  started: string;
+  ended?: string;
+};
+const periodSerializer = R.evolve({
+  started: toISO,
+  ended: optional(toISO),
+});
+
+export const educationSerializer = R.map<Education, SerializedEducation>(
+  periodSerializer
 );
 
-export const educationDeserializer = R.map(
+export const educationDeserializer = R.map<SerializedEducation, Education>(
   R.evolve({
     started: DateTime.fromISO,
     ended: DateTime.fromISO,
   })
 );
 
-export const courseSerializer = R.map(
+type SerializedCourse = Omit<SerializedEducation, 'institution'>;
+type SerializedCourses = {
+  institution: string;
+  courses: SerializedCourse[];
+};
+
+export const courseSerializer = R.map<Courses, SerializedCourses>(
   R.evolve({
-    courses: educationSerializer,
+    courses: R.map<Omit<Education, 'institution'>, SerializedCourse>(
+      periodSerializer
+    ),
   })
 );
+
+export type SerializedResume = Omit<
+  Resume,
+  'jobs' | 'educations' | 'courses'
+> & {
+  jobs: SerializedJob[];
+  educations: SerializedEducation[];
+  courses: SerializedCourses[];
+};
+
+const contactsSerializer = R.map(
+  R.compose<[Contact], Contact, Contact>(
+    R.pick(['link', 'name', 'username', 'icon']),
+    R.evolve({
+      icon: icon => (icon?.name ? { ...icon } : {}),
+    })
+  )
+);
+
+export const resumeSerializer = R.evolve({
+  contacts: contactsSerializer,
+  jobs: jobSerializer,
+  educations: educationSerializer,
+  courses: courseSerializer,
+});
