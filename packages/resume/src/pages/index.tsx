5f0565139f228d7add69655d3f6a4101c0db64f1
Peter <github@peter.dev.br>
2022-01-19 23:28:57 -0300

    refactor(resume): home page props

--- packages/resume/src/pages/index.tsx
+++ packages/resume/src/pages/index.tsx
@@ -5,62 +5,59 @@ import getResume from 'services';
 import { job, education, resume as serializer } from 'lib/serializers';
 import styles from '../styles/Home.module.scss';
 
-type HomeProps = serializer.SerializedResume;
+type HomeProps = { resume: serializer.SerializedResume };
 
 const Home: NextPage<HomeProps> = ({
-  bio,
-  contacts,
-  skills,
-  jobs,
-  educations,
-  courses,
-}) => (
-  <div className={styles.container}>
-    <Head>
-      <title>Peter&apos;s Résumé</title>
-      <meta name="description" content="Peter's Résumé" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      {/* <link rel="icon" href="/favicon.ico" /> */}
-    </Head>
-    <Header />
-    <main className={styles.main}>
-      <div className={styles.profile}>
-        <Section title="Profile" fill>
-          <p>{bio}</p>
+  resume: { contacts, bio, jobs, educations, courses, skills },
+}) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Peter&apos;s Résumé</title>
+        <meta name="description" content="Peter's Résumé" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+      <Header />
+      <main className={styles.main}>
+        <div className={styles.profile}>
+          <Section title="Profile" fill>
+            <p>{bio}</p>
+          </Section>
+          <ContactsList contacts={contacts} />
+        </div>
+        <Section title="Experience">
+          <Timeline jobs={job.deserialize(jobs)} />
         </Section>
-        <ContactsList contacts={contacts} />
-      </div>
-      <Section title="Experience">
-        <Timeline jobs={job.deserialize(jobs)} />
-      </Section>
-      <Section title="Education">
-        {education
-          .deserialize(educations)
-          .map(({ title, institution: { name: location } }) => (
-            <div key={title}>
-              <h2>{title}</h2>
-              <p>{location}</p>
+        <Section title="Education">
+          {education
+            .deserialize(educations)
+            .map(({ title, institution: { name: location } }) => (
+              <div key={title}>
+                <h2>{title}</h2>
+                <p>{location}</p>
+              </div>
+            ))}
+        </Section>
+        <Section title="Courses & Training">
+          {courses.map(({ courses: trainings, institution }) => (
+            <div key={institution}>
+              <h3>{institution}</h3>
+              <ul>
+                {trainings.map(({ title }) => (
+                  <li key={title}>{title}</li>
+                ))}
+              </ul>
             </div>
           ))}
-      </Section>
-      <Section title="Courses & Training">
-        {courses.map(({ courses: trainings, institution }) => (
-          <div key={institution}>
-            <h3>{institution}</h3>
-            <ul>
-              {trainings.map(({ title }) => (
-                <li key={title}>{title}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </Section>
-      <Section title="Skills">
-        <Skills skills={skills} />
-      </Section>
-    </main>
-  </div>
-);
+        </Section>
+        <Section title="Skills">
+          <Skills skills={skills} />
+        </Section>
+      </main>
+    </div>
+  );
+};
 
 export default Home;
 
@@ -74,6 +71,6 @@ export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   }
 
   return {
-    props: serializer.serialize(resume),
+    props: { resume: serializer.serialize(resume) },
   };
 };
