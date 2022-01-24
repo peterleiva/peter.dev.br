f22e84fab8ed5c11700719294f699d92fe691040
Peter <github@peter.dev.br>
2022-01-24 13:08:57 -0300

    refactor(resume): don't refetchOnWindoFocus in query

--- packages/resume/src/QueryProvider.tsx
+++ packages/resume/src/QueryProvider.tsx
@@ -1,11 +1,23 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-const client = new QueryClient();
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 type QueryClientProviderProps = {
   children: React.ReactNode | undefined;
 };
 
 export default function QueryProvider({ children }: QueryClientProviderProps) {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
+  );
 }
