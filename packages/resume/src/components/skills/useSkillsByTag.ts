efbf63a291441f48877d247d75c9c3b52fcf7274
Peter <github@peter.dev.br>
2022-01-24 13:39:02 -0300

    feat(resume): add hook to fetch skills by tag

---
+++ packages/resume/src/components/skills/useSkillsByTag.ts
@@ -0,0 +1,33 @@
+import { QueryFunctionContext, useQuery } from 'react-query';
+import type { Skill, Tag } from 'types';
+import { getJsonOrThrow } from './helpers';
+
+type QueryKey = ['skills', Tag | undefined];
+
+const fetchSkillByTag = ({
+  queryKey: [, tag],
+}: QueryFunctionContext<QueryKey>) => {
+  if (!tag) {
+    return;
+  }
+  const url = `/api/tags/${tag}/skills`;
+
+  return fetch(url).then(getJsonOrThrow);
+};
+
+/**
+ * Get all Skills by tag or all when none is specified
+ *
+ * @param tag
+ * @returns
+ */
+export default function useSkillsByTag(tag?: Tag) {
+  const { data, ...query } = useQuery<
+    Skill | undefined,
+    unknown,
+    Skill[],
+    QueryKey
+  >(['skills', tag], fetchSkillByTag);
+
+  return { skills: data, ...query };
+}
