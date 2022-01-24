051319a6fe3eabd55a9c7320e75f1b4e618b8db1
Peter <github@peter.dev.br>
2022-01-24 13:09:30 -0300

    feat(resume): fetch polyfill

--- packages/resume/src/pages/_app.tsx
+++ packages/resume/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import React from 'react';
 import QueryProvider from 'QueryProvider';
 import { Analytics } from 'components';
+import 'whatwg-fetch';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
