7e80d3be2923a21e5de126f53ffa03327678465d
Peter <github@peter.dev.br>
2022-01-26 09:36:40 -0300

    chore(resume): setup sentry

--- packages/resume/next.config.js
+++ packages/resume/next.config.js
@@ -1,4 +1,19 @@
+const { withSentryConfig } = require('@sentry/nextjs');
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+const config = {
   reactStrictMode: true,
 };
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+  silent: true, // Suppresses all logs
+};
+
+module.exports = withSentryConfig(config, sentryWebpackPluginOptions);
