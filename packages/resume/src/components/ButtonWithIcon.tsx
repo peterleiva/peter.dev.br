677b4acb3f7723f676c85868d78b76480d94a3ef
Peter <github@peter.dev.br>
2022-02-03 14:36:50 -0300

    refactor(resume): transform Button in HOC

--- packages/resume/src/components/ButtonWithIcon.tsx
+++ packages/resume/src/components/ButtonWithIcon.tsx
@@ -1,28 +1,25 @@
 import { IconType } from 'react-icons';
 import Button from './Button';
 
-type ButtonIconProps = Parameters<typeof Button>[0] & {
-  Icon: IconType;
-};
+export default function ButtonWithIcon(Icon: IconType) {
+  return function WithIcon({
+    children,
+    ...buttonProps
+  }: $ElementProps<typeof Button>) {
+    return (
+      <Button {...buttonProps}>
+        <span className="button">
+          {children} <Icon size={26} />
+        </span>
 
-export default function ButtonIcon({
-  children,
-  Icon,
-  ...buttonProps
-}: ButtonIconProps) {
-  return (
-    <Button {...buttonProps}>
-      <span className="button">
-        {children} <Icon size={26} />
-      </span>
-
-      <style jsx>{`
-        .button {
-          display: flex;
-          gap: var(--space-sm);
-          alignitems: center;
-        }
-      `}</style>
-    </Button>
-  );
+        <style jsx>{`
+          .button {
+            display: flex;
+            gap: var(--space-sm);
+            alignitems: center;
+          }
+        `}</style>
+      </Button>
+    );
+  };
 }
--- packages/resume/src/globals.d.ts
+++ packages/resume/src/globals.d.ts
@@ -1,6 +1,13 @@
 /* eslint-disable no-var */
 import type { Connection } from 'mongoose';
+import React from 'react';
 
 declare global {
   var db: Connection | undefined;
+
+  declare type $ElementProps<T> = T extends React.ComponentType<infer Props>
+    ? Props extends object
+      ? Props
+      : never
+    : never;
 }
