bc8bb83bc76184ed095e1ad52e8db4d106bdbd39
Peter <github@peter.dev.br>
2022-02-03 17:39:56 -0300

    feat(resume): add form field clear button

--- packages/resume/src/components/form/Input.tsx
+++ packages/resume/src/components/form/Input.tsx
@@ -1,38 +1,55 @@
 import clsx from 'clsx';
-import { FocusEventHandler, useRef, useState } from 'react';
+import { FocusEventHandler, RefObject, useRef, useState } from 'react';
 import type { IconType } from 'react-icons';
 import {
   BiData as FallbackIcon,
   BiMessageSquareDetail as TextareaFallback,
 } from 'react-icons/bi';
 import { FiAlertOctagon as InvalidIcon } from 'react-icons/fi';
+import { GrClose as CloseIcon } from 'react-icons/gr';
 import styles from './Input.module.scss';
 
 type InputProps = JSX.IntrinsicElements['input'];
 type TextareaProps = JSX.IntrinsicElements['textarea'];
+type InputElement = HTMLInputElement | HTMLTextAreaElement;
 
 type FormInputProps = InputProps | TextareaProps;
 
 type Props = FormInputProps & {
   Icon?: IconType;
   id: string;
+  onClear?: () => void;
+  showClear?: boolean;
 };
 
 const isInput = (props: FormInputProps): props is InputProps =>
   'type' in props || 'accept' in props;
 
-export default function Input({ Icon, id, className, ...inputProps }: Props) {
+export default function Input({
+  Icon,
+  id,
+  className,
+  onClear,
+  showClear = false,
+  ...inputProps
+}: Props) {
   const inputCls = clsx(styles.input, className);
-  const inputRef = useRef<HTMLInputElement>(null);
   const [invalid, setInvalid] = useState<boolean>(false);
+  const ref = useRef<InputElement>(null);
 
-  const handleBlur: FocusEventHandler<
-    HTMLInputElement | HTMLTextAreaElement
-  > = (e): void => {
-    setInvalid(!e.target?.validity?.valid);
+  const setValidity = (element?: InputElement | null) =>
+    setInvalid(!element?.validity?.valid);
+
+  const handleBlur: FocusEventHandler<InputElement> = (e): void => {
+    setValidity(e.target);
   };
 
   const handleInvalid = () => setInvalid(true);
+
+  const handleClear = () => {
+    onClear?.();
+    setTimeout(() => setValidity(ref?.current), 0);
+  };
 
   const printIcon = (Fallback: IconType = FallbackIcon) => (
     <span className={styles.icon}>
@@ -48,9 +65,15 @@ export default function Input({ Icon, id, className, ...inputProps }: Props) {
 
   if (isInput(inputProps)) {
     return (
-      <>
+      <div className={styles['input-container']}>
+        {showClear && (
+          <CloseIcon
+            className={clsx(styles.clear, styles.right, styles.top)}
+            onClick={handleClear}
+          />
+        )}
         <input
-          ref={inputRef}
+          ref={ref as RefObject<HTMLInputElement>}
           type="text"
           className={inputCls}
           id={id}
@@ -60,21 +83,28 @@ export default function Input({ Icon, id, className, ...inputProps }: Props) {
           {...inputProps}
         />
         {printIcon()}
-      </>
+      </div>
     );
   }
 
   return (
-    <>
+    <div className={styles['input-container']}>
+      {showClear && (
+        <CloseIcon
+          className={clsx(styles.clear, styles.bottom, styles.right)}
+          onClick={onClear}
+        />
+      )}
       <textarea
         name={id}
         id={id}
+        ref={ref as RefObject<HTMLTextAreaElement>}
         className={clsx(inputCls, styles.textarea)}
         onBlur={handleBlur}
         onInvalid={handleInvalid}
         {...(inputProps as TextareaProps)}
       />
       {printIcon(TextareaFallback)}
-    </>
+    </div>
   );
 }
