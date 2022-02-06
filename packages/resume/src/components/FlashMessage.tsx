784b502ae3f2f4cdf16a7700fa949f90a07d57e4
Peter <github@peter.dev.br>
2022-02-06 15:46:31 -0300

    test(resume): test FlashMessage component

--- packages/resume/src/components/FlashMessage.tsx
+++ packages/resume/src/components/FlashMessage.tsx
@@ -45,68 +45,67 @@ const FlashMessage = (Icon: IconType, type: MessageType) =>
     autoClose,
     className,
   }: FlashMessageProps) {
-    const [show, setShow] = useState(!hide);
-    const setHide = () => setShow(false);
+    const [closed, setClosed] = useState(hide);
+
+    const close = () => setClosed(true);
 
     if (autoClose) {
-      debounce(() => setShow(false), autoClose);
+      debounce(close, autoClose);
     }
 
     const color = colors[type];
 
-    if (show) {
-      return (
-        <div className={clsx('flash-message', className)}>
-          <button className="close reset-button" onClick={setHide}>
-            <CloseIcon size="24" />
-          </button>
-          <span className="alert">
-            <svg width="43" height="48" xmlns="http://www.w3.org/2000/svg">
-              <path d="M21.37 0a21.37 21.37 0 1 1-4.13 42.35l-7.86 5.19a2 2 0 0 1-3.1-1.8l.52-8.73A21.37 21.37 0 0 1 21.37 0Z" />
-              <g transform="translate(13, 15)">
-                <Icon size="18" />
-              </g>
-            </svg>
-          </span>
-          <div className="body">
-            <h2 className="type-2">{title}!</h2>
-            <p>{children}</p>
-          </div>
+    if (closed) return null;
 
-          <style jsx>{`
-            .flash-message {
-              position: relative;
-              color: var(--color-white);
-              padding: var(--space);
-              border-radius: var(--border-radius-xl);
-              background: no-repeat url(/flash-message-bg.svg) ${color.primary};
-              background-position: left -14px bottom -25px;
-              background-blend-mode: overlay;
-            }
-
-            .body {
-              margin-left: var(--space-lg);
-            }
-
-            .alert {
-              position: absolute;
-              top: calc(-1 * var(--space));
-              left: var(--space);
-            }
-
-            .alert path {
-              fill: ${color.secondary};
-            }
-
-            .close {
-              float: right;
-            }
-          `}</style>
+    return (
+      <div className={clsx('flash-message', className)}>
+        <button className="close reset-button" onClick={close}>
+          <CloseIcon size="24" />
+        </button>
+        <span className="alert">
+          <svg width="43" height="48" xmlns="http://www.w3.org/2000/svg">
+            <path d="M21.37 0a21.37 21.37 0 1 1-4.13 42.35l-7.86 5.19a2 2 0 0 1-3.1-1.8l.52-8.73A21.37 21.37 0 0 1 21.37 0Z" />
+            <g transform="translate(13, 15)">
+              <Icon size="18" />
+            </g>
+          </svg>
+        </span>
+        <div className="body">
+          <h2 className="type-2">{title}!</h2>
+          <p>{children}</p>
         </div>
-      );
-    }
 
-    return null;
+        <style jsx>{`
+          .flash-message {
+            position: relative;
+            color: var(--color-white);
+            padding: var(--space);
+            border-radius: var(--border-radius-xl);
+            background: no-repeat url(/flash-message-bg.svg) ${color.primary};
+            background-position: left -14px bottom -25px;
+            background-blend-mode: overlay;
+          }
+
+          .body {
+            margin-left: var(--space-lg);
+          }
+
+          .alert {
+            position: absolute;
+            top: calc(-1 * var(--space));
+            left: var(--space);
+          }
+
+          .alert path {
+            fill: ${color.secondary};
+          }
+
+          .close {
+            float: right;
+          }
+        `}</style>
+      </div>
+    );
   };
 
 export const SuccessMessage = FlashMessage(SuccessIcon, 'success');
