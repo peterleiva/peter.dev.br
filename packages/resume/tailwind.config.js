08dae6a6b5328021ecf88d7b1c7f5507ace8880b
Peter <github@peter.dev.br>
2022-02-08 19:13:44 -0300

    refactor(resume): button styles with tailwind

--- packages/resume/tailwind.config.js
+++ packages/resume/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ['./src/**/*.{ts,tsx,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontWeight: {
+        normal: 340,
+        medium: 400,
+        semibold: 500,
+      },
+    },
   },
   plugins: [],
 };
