6a2c40878b649ebc1d8a37392078c172d3dad9b5
Peter <github@peter.dev.br>
2022-02-23 17:16:26 -0300

    refactor(resume): create Label form component

---
+++ packages/resume/src/form/Label.tsx
@@ -0,0 +1,25 @@
+import clsx from 'clsx';
+
+type Props = {
+  label: string;
+  id: string;
+  required?: boolean;
+  className?: string;
+};
+
+export default function Label({ id, label, required, className }: Props) {
+  return (
+    <label
+      htmlFor={id}
+      className={clsx(
+        'font-semibold',
+        {
+          "after:content-['*'] after:text-red-500": required,
+        },
+        className
+      )}
+    >
+      {label}
+    </label>
+  );
+}
