941c550d4fca63bf29e6ebc17926fcce3cdae0a2
Peter <github@peter.dev.br>
2022-02-23 15:56:50 -0300

    fix(resume): font sizes using tailwind

--- packages/resume/src/components/layout/Header.tsx
+++ packages/resume/src/components/layout/Header.tsx
@@ -7,39 +7,19 @@ type HeaderProps = {
 
 export default function Header({ name, jobTitle }: HeaderProps) {
   return (
-    <header>
+    <header className="md:flex flex-nowrap justify-between items-center text-center py-8">
       <section className="title">
-        <h1>{name}</h1>
-        <h2>{jobTitle}</h2>
+        <h1 className="font-semibold text-6xl">{name}</h1>
+        <h2 className="rounded bg-neutral-800 text-slate-300 px-4 py-2 font-bold">
+          {jobTitle}
+        </h2>
       </section>
       <NavBar />
 
       <style jsx>{`
-        header {
-          padding-block: var(--space-md) 0;
-          text-align: center;
-        }
-
         .title > h2 {
           margin-left: -20%;
           margin-right: -10%;
-        }
-
-        @media screen and (min-width: 866px) {
-          header {
-            display: flex;
-            flex-flow: row nowrap;
-            justify-content: space-between;
-            align-items: center;
-          }
-        }
-
-        h2 {
-          color: var(--color-white);
-          background: var(--color-black);
-          border-radius: var(--border-radius);
-          padding: var(--space-xs) var(--space);
-          font-variant: small-caps;
         }
       `}</style>
     </header>
