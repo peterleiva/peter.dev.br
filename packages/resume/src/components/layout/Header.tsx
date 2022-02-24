4fafdfb8965581ee3dcd952a07159b42eb1a4f7b
Peter <github@peter.dev.br>
2022-02-23 22:09:11 -0300

    feat(resume): add home page link to header's name

--- packages/resume/src/components/layout/Header.tsx
+++ packages/resume/src/components/layout/Header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import NavBar from './NavBar';
 
 type HeaderProps = {
@@ -9,15 +10,19 @@ export default function Header({ name, jobTitle }: HeaderProps) {
   return (
     <header className="md:flex flex-nowrap justify-between items-center text-center py-8">
       <section className="title">
-        <h1 className="font-semibold text-6xl">{name}</h1>
-        <h2 className="rounded bg-neutral-800 text-slate-300 px-4 py-2 font-bold">
-          {jobTitle}
-        </h2>
+        <Link href="/" passHref>
+          <a href="dummy">
+            <h1 className="font-semibold text-6xl text-neutral-800">{name}</h1>
+            <h2 className="rounded bg-neutral-800 text-slate-300 px-4 py-2 font-bold">
+              {jobTitle}
+            </h2>
+          </a>
+        </Link>
       </section>
       <NavBar />
 
       <style jsx>{`
-        .title > h2 {
+        .title h2 {
           margin-left: -20%;
           margin-right: -10%;
         }
