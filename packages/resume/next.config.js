4d7e1c26a8d7b7ae17b384a6aef78da6d5a0f6e9
Peter <github@peter.dev.br>
2022-02-24 23:08:03 -0300

    feat(resume): setup next-i18next

--- packages/resume/next.config.js
+++ packages/resume/next.config.js
@@ -1,9 +1,11 @@
 const { withSentryConfig } = require('@sentry/nextjs');
+const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n,
 };
 
 const sentryWebpackPluginOptions = {
