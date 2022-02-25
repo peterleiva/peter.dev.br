4be679bf1b89374ccfdc972ef80e7388375c341b
Peter <github@peter.dev.br>
2022-02-24 21:38:03 -0300

    feat(resume): block search engine index

--- packages/resume/src/components/layout/Layout.tsx
+++ packages/resume/src/components/layout/Layout.tsx
@@ -16,6 +16,7 @@ export default function Layout({ name, children, jobTitle, Top }: Props) {
     <div className={styles.container}>
       <Head>
         <title>{name}&apos;s Résumé</title>
+        <meta name="robots" content="noindex" />
         <meta name="description" content={`${name}'s Resumé`} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
