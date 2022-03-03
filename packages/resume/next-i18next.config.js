63eac13a43937983e1bb65ab9d9c576e2c948123
Peter <github@peter.dev.br>
2022-03-03 19:11:16 -0300

    fix(resume) :remove locales domains

--- packages/resume/next-i18next.config.js
+++ packages/resume/next-i18next.config.js
@@ -5,19 +5,6 @@ module.exports = {
     defaultLocale: 'en',
     locales: ['en', 'pt'],
 
-    domains: [
-      {
-        domain: 'curriculo.peter.dev.br',
-        locales: ['en', 'pt'],
-        defaultLocale: 'pt',
-      },
-      {
-        domain: 'resume.peter.dev.br',
-        locales: ['en', 'pt'],
-        defaultLocale: 'en',
-      },
-    ],
-
     reloadOnPrerender: process.env.NODE_ENV === 'development',
 
     localePath: path.resolve('./public/locales'),
