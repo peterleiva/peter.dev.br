2e6740dd71084f4a16989fb6ee8e591ebb27ef1e
Peter <github@peter.dev.br>
2022-03-03 18:56:51 -0300

    fix(resume): revert get education service

--- packages/resume/src/services/get-educations.ts
+++ packages/resume/src/services/get-educations.ts
@@ -1,23 +1,48 @@
-import { pick } from 'ramda';
+import { evolve, map } from 'ramda';
 import type { Education } from 'types';
 import { toDateTime, optionalToDateTime } from 'lib';
-import { type EducationDocument } from './models/education';
+import EducationModel from './models/education';
 import { ResumeDocument } from './models/resume';
-import locale from './locale';
+
+type EducationAggregation = Omit<Education, 'started' | 'ended'> & {
+  started: Date;
+  ended?: Date;
+};
 
 export default async function getEducations(
   resume: ResumeDocument
 ): Promise<Education[]> {
-  const { educations } = await resume.populate<{
-    educations: EducationDocument[];
-  }>('educations');
+  const educations = await EducationModel.aggregate<EducationAggregation>([
+    {
+      $match: {
+        _id: { $in: resume.educations },
+      },
+    },
+    {
+      $sort: {
+        started: -1,
+        ended: -1,
+        title: 1,
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        title: 1,
+        institution: {
+          name: '$institution.name',
+        },
+        description: 1,
+        started: 1,
+        ended: 1,
+      },
+    },
+  ]).exec();
 
-  return educations.map((edu: EducationDocument) => {
-    return {
-      ...pick(['title', 'description'], edu.translate(locale.getLocale())),
-      institution: pick(['name', ''], edu.institution),
-      started: toDateTime(edu.started),
-      ended: optionalToDateTime(edu.ended),
-    };
-  });
+  return map(
+    evolve({
+      started: toDateTime,
+      ended: optionalToDateTime,
+    })
+  )(educations);
 }
