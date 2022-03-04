d12b7a4854d6a25fbe011aa69adf2ab476a543d1
Peter <github@peter.dev.br>
2022-03-03 23:17:54 -0300

    fix(resume): html page title

--- packages/resume/src/components/layout/Layout.tsx
+++ packages/resume/src/components/layout/Layout.tsx
@@ -4,6 +4,7 @@ import Footer from './Footer';
 import Header from './Header';
 import styles from 'styles/Home.module.scss';
 import LanguageSwitcher from '../LanguageSwitcher';
+import { useTranslation } from 'next-i18next';
 
 type Props = {
   name: string;
@@ -13,12 +14,14 @@ type Props = {
 };
 
 export default function Layout({ name, children, jobTitle, Top }: Props) {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>{name}&apos;s Résumé</title>
+        <title>{t('resume_title', { name })}</title>
         <meta name="robots" content="noindex" />
-        <meta name="description" content={`${name}'s Resumé`} />
+        <meta name="description" content={t('resume_title', { name })} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
